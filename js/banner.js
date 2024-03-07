aed92052f24c2af0356ac7572a7690a0ece5fd58
Muhammad Shaheer <shaheer@uptek.com>
2024-03-08 02:21:01 +0500

    sections

--- js/banner.js
+++ js/banner.js
@@ -1,12 +1,14 @@
 const progressCircle = document.querySelector(".banner .autoplay-progress svg");
 const progressContent = document.querySelector(".banner .autoplay-progress span");
+const playPausebButton = document.querySelector(".play-pause");
 
 if (progressCircle && progressContent) {
-  new Swiper(".banner__slider", {
+  const bannerSlider = new Swiper(".banner__slider", {
     centeredSlides: true,
+    parallax: true,
+    speed: 600,
     autoplay: {
-      delay: 2000,
-      disableOnInteraction: false
+      delay: 2000
     },
     pagination: {
       el: ".swiper-pagination",
@@ -21,6 +23,17 @@ if (progressCircle && progressContent) {
         progressCircle.style.setProperty("--progress", 1 - progress);
         progressContent.textContent = `${Math.ceil(time / 1000)}s`;
       }
+    }
+  });
+
+  playPausebButton.addEventListener('click', () => {
+    playPausebButton.classList.toggle('play-pause__play')
+    playPausebButton.classList.toggle('play-pause__pause')
+
+    if (bannerSlider.autoplay.running) {
+      bannerSlider.autoplay.stop();
+    } else {
+      bannerSlider.autoplay.start();
     }
   });
 }

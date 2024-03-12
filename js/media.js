b78793cc3599227aa9c8d450d79dbc1e55e6df63
Muhammad Shaheer <shaheer@uptek.com>
2024-03-12 23:07:36 +0500

    mobile version

--- js/media.js
+++ js/media.js
@@ -2,11 +2,22 @@ let mediaSliderEl = document.querySelector('.media__slider');
 
 if (mediaSliderEl) {
   new Swiper(mediaSliderEl, {
-    slidesPerView: 6,
+    slidesPerView: 1,
     spaceBetween: 20,
       autoplay: {
       delay: 2500,
       disableOnInteraction: false,
-    }
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3
+      },
+      1024: {
+        slidesPerView: 6,
+      },
+    },
   });
 };
--- js/testimonials.js
+++ js/testimonials.js
@@ -2,13 +2,21 @@ let testimonialSliderEl = document.querySelector('.testimonial__slider');
 
 if (testimonialSliderEl) {
   new Swiper(testimonialSliderEl, {
-    slidesPerView: 3,
+    slidesPerView: 1,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3,
+      },
     }
   });
 }
